4b5427e7e3b82d46ca925ce221ffe2c472604beb
Marcell <47815117+GumboJack@users.noreply.github.com>
2020-05-02 11:36:05 +0200

    GUI készítés

---
+++ src/main/java/BreweryClient/ClientGUI.java
@@ -0,0 +1,10 @@
+package BreweryClient;
+
+import javax.swing.*;
+
+public class ClientGUI {
+    private JPanel StoreListPanel;
+    private JPanel mainPanel;
+    private JPanel beerPanel;
+    private JPanel beerInputPanel;
+}
